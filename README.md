4c7bc322f3ea892c4601aebb27dca154acf05817
pomke <pomke@pomke.com>
2015-10-19 02:46:49 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,141 @@
 # rukus-signal
+
 signalling for rukus apps
+
+
+
+
+### Usage:
+
+In your rukus app's index.js, create a signals instance on your App object:
+
+```js 
+var Signal = require('rukus-signal');
+RukusApp.signals = new Signal(RukusApp, riot);
+```
+
+You can then use RukusApp.signals to connect views to things like a store:
+
+#### A simple todo store which emits an updated signal and accepts a new todo.
+```js
+function TodoStore() {
+    RukusApp.signals.init(this);
+
+    this.todos = [];
+
+    this.emits({   
+        signal : 'updated',
+        desc : `signaled when the TodoStore has changed, observers should watch
+                for this event if they are interested in TodoStore updates.`,
+        data : 'null'
+        });
+
+    this.accepts({ 
+        signal : 'new',
+        desc : 'adds a new Todo to the store',
+        data : 'A Todo object',
+        func : (todo) => {
+            this.todos.push(todo);
+            this.emit('updated');
+        }});
+};
+
+RukusApp.todoStore = new TodoStore();
+```
+
+
+#### A component which renders and adds todos:
+
+```js
+module.exports = function(opts) {
+    RukusApp.signals.init(this);
+
+    this.binds({ 'todoStore:updated' : this.update });
+    this.invokes({'new' : 'todoStore:new'});
+
+    this.add = () => {
+        this.invoke('new', { todo : this.todo.value });
+    };
+};
+```
+
+
+
+## API
+
+##### Signal.init(this)
+
+Initialises an object or component handler to use rukus signals.
+
+##### Signal.doc() -> JSON
+
+Returns a JSON structure which documents every controller and component which
+have registered with this rukus singnal instance, and who communicates with 
+who.
+
+##### this.accepts(sig1, sig2, ...)
+
+Takes one or more signal definitions, stating that this object accepts these
+signals and acts on them. signal definitions must be in the format:
+
+```js
+{ 
+    signal : 'signalName',
+    desc : `a description of what this signal does.`,
+    data : `a description of the data it requires.`,
+    func : [Function] // which will be called if this signal is recieved.
+}
+```
+
+##### this.binds(signals)
+
+Takes an object of signal paths to functions and binds them to this object.
+
+A signal path is a string in the format of:
+
+```
+'dotted.path.to.object.under.RukusApp:signalName' 
+```
+
+Eg:
+
+```js
+{ 'todoStore:updated', this.update }
+```
+
+In the event that the todoStore signals updated, call this.update.
+
+
+##### this.emits(sig1, sig2, ...)
+
+Takes one or more signal definitions, stating that this object emits these
+signals. signal definitions must be in the format:
+
+```js
+{ 
+    signal : 'signalName',
+    desc : `a description of what this signal means.`,
+    data : `a description of the data it sends.`,
+}
+```
+
+##### this.emit(signalName, data)
+
+Emit a signal by name as defined by this.emits. Optionally emit data.
+
+##### this.invokes(signals)
+
+
+Takes an object of local name to signal paths on other objects, indicating that
+this object intends to invoke their signals.
+
+
+```js
+{ 'newTodo' : 'todoStore:updated', ... }
+```
+
+This object may now invoke 'newTodo'.
+
+##### this.invoke(signalName, data)
+
+Invoke a signal by name, as defined via this.invokes.
---
+++ index_src.js
@@ -0,0 +1,148 @@
+module.exports = RukusSignal;
+
+
+function dots(obj) {
+    /* takes an object and returns a getter which takes a dot path, ie:
+     * var find = dots({'a' : { b : 'woo'}});
+     * find('a.b') -> 'woo'
+     */
+    return function(path) {
+        return path.split('.').reduce(
+            function(prv, i) {
+                return prv !== null ? (prv[i] || null) : null;
+            }, obj);
+    };
+}
+
+function RukusSignal(RukusApp, riot) {
+    if(!RukusApp.__find) RukusApp.__find = dots(RukusApp);
+    let signalBox = this;
+    signalBox.counter = 0;
+    signalBox.controllers = {};
+    signalBox.views = {};
+
+    this.doc = function() {
+        return {};
+    };
+
+    this.init = function(obj) {
+
+        obj.__emits = {};   // Signals I can emit
+        obj.__accepts = {}; // Signals I'll accept
+        obj.__binds = {};   // Other's emits I want to bind to
+        obj.__invokes = {}; // Other's accepts I will invoke
+
+        if(!obj.on) riot.observable(obj); // ensure obj is observable
+        if(obj.root) {
+            // we're a view component
+            obj.__sigKind = 'view';
+            obj.__sigName = obj.root.tagName || 
+                'unkownComponent.'+ (++signalBox.counter);
+            signalBox.views[obj.__sigName] = obj;
+
+            // for view components, bind when we are mounted and unbind
+            // when we are unmounted:
+            obj.on('mount', () => {
+                Object.keys(obj.__binds).forEach(function(sigStr) {
+                    let [path, sig] = sigStr.split(':');
+                    let obs = RukusApp.__find(path);
+                    if(!obs) return console.error(`cannot find ${path} to bind`);
+                    obs.on(sig, obj.__binds[sigStr]);
+                });
+            });
+
+            obj.on('unmount', () => {
+                Object.keys(obj.__binds).forEach(function(sigStr) {
+                    let [path, sig] = sigStr.split(':');
+                    let obs = RukusApp.__find(path);
+                    if(!obs) return console.error(`cannot find ${path} to unbind`);
+                    obs.off(sig, obj.__binds[sigStr]);
+                });
+            });
+
+        } else {
+            // we're a controller / store
+            obj.__sigKind = 'controller';
+            obj.__sigName = ++signalBox.counter;
+            signalBox.controllers[obj.__sigName] = obj;
+        }
+
+        // our own signals that we will emit (public API)
+        // takes one or more:
+        //
+        // { signal : 'name',
+        //   desc : 'sig description',
+        //   data : 'data emitted description'
+        // }
+        obj.emits = (...signals) => {
+            signals.forEach(function(sig) {
+                obj.__emits[sig.signal] = sig;
+            });
+        };
+
+        // emits a signal defined with this.emits by name, optionally with data
+        obj.emit = (sigName, data) => {
+            if(!obj.__emits[sigName]) {
+                return console.error(
+                    `${sigName} is not declared with this.emits()`);
+            }
+            obj.trigger(sigName, data);
+        };
+
+
+        // our own signals that we'll respond to (public API)
+        // takes one or more:
+        //
+        // { signal : 'name',
+        //   desc : 'sig description',
+        //   data : 'data accepted description',
+        //   func : function to call
+        // }
+        obj.accepts = (...signals) => {
+            signals.forEach(function(sig) {
+                obj.__accepts[sig.signal] = sig;
+                obj.on(sig.signal, sig.func);
+            });
+        };
+
+        // Other's signals that we'll bind too, takes:
+        //
+        // { 'dot.path.under.RukusApp:signalName' : func }
+        obj.binds = (signals) => {
+            Object.keys(signals).forEach(function(sigPath) {
+                obj.__binds[sigPath] = signals[sigPath];
+                //If we are a controller, bind immediately, views will
+                //be bound and unbound when their tag is mounted and unmounted
+                if(obj.__sigKind == 'controller') {
+                    let [path, sig] = sigPath.split(':');
+                    let obs = RukusApp.__find(path);
+                    if(!obs) return console.error(`cannot find ${path} to bind`);
+                    obs.off(sig, signals[sigPath]);
+                }
+            });
+        };
+
+        // Other's 'accepts' that we'll invoke, takes:
+        //
+        // { 'localSignalName' : 'dot.path.under.RukusApp:signalName' }
+        obj.invokes = (signals) => {
+            Object.keys(signals).forEach(function(localName) {
+                obj.__invokes[localName] = signals[localName];
+            });
+        };
+ 
+        // Invoke something defined with this.invokes(), takes:
+        //
+        // localSignalName, optional data
+        obj.invoke = (sigName, data) => {
+            let sigPath = obj.__invokes[sigName];
+            if(!sigPath) console.error(`${sigName} not declared with this.invokes()`);
+            let [path, sig] = sigPath.split(':');
+            let obs = RukusApp.__find(path);
+            if(!obs) return console.error(`cannot find ${path} to invoke`);
+            console.log('triggering', sig, 'on ', obs);
+            obs.trigger(sig, data);
+        };
+    };
+
+};
